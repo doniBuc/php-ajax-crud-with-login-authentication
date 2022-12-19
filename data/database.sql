7c56a95e701db97a76ee726a500db5b5566d2101
redpangilinan <redpangilinan715@gmail.com>
2022-12-20 06:53:43 +0800

    Included database records

--- data/database.sql
+++ data/database.sql
@@ -1,59 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 16, 2022 at 10:13 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `php_scrud`
---
 CREATE DATABASE IF NOT EXISTS `php_scrud` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `php_scrud`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_accounts`
---
-
-CREATE TABLE IF NOT EXISTS `tb_accounts` (
-  `account_id` int(10) NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(65) NOT NULL,
-  `creation_date` date NOT NULL,
-  `account_type` varchar(15) NOT NULL,
-  PRIMARY KEY (`account_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_records`
---
-
-CREATE TABLE IF NOT EXISTS `tb_records` (
-  `record_id` int(10) NOT NULL AUTO_INCREMENT,
-  `record` varchar(60) NOT NULL,
-  `details` int(255) NOT NULL,
-  `creation_date` date NOT NULL,
-  PRIMARY KEY (`record_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
